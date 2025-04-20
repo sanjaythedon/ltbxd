ebc61a42a6f9a9740371d5983f2c63cc037e92b3
aaa <shanjairajan54@gmail.com>
2025-04-20 14:30:37 +0530

    changed the way env vars are imported

--- logfire_definition.py
+++ logfire_definition.py
@@ -1,9 +1,11 @@
 import os
+from dotenv import load_dotenv
 import logfire
+
+# Load environment variables from .env file
+load_dotenv()
 
 logfire.configure(
     scrubbing=False,
     token=os.getenv("PYDANTIC_TOKEN")
 )
-
-
